ecfe5ebcfaa142b6d2bbf8db4ca06b6ff4e4073c
vacekj <vacekj@outlook.com>
2021-03-20 22:11:23 +0100

    inital commit

---
+++ config/env/production/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({env}) => ({
+  upload: {
+    provider: "sftp-v2",
+    providerOptions: {
+      host: "cdn.kryptokurzy.cz",
+      port: "22",
+      user: "cdn",
+      password: process.env.SFTP_PASSWORD,
+      basePath: "/cdn",
+      baseUrl: "https://cdn.kryptokurzy.cz/"
+    }
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'abbddee9-5416-43fe-8017-a5fa9f385dc1'
+};
